ea633ad075b1055ce8c249d8900e1f9530b3004e
Dietmar Seifert <dietmar.seifert@nvbw.de>
2026-02-07 12:03:06 +0100

    update path to properties-File

--- src/main/java/de/nvbw/base/BFRKApiApplicationconfiguration.java
+++ src/main/java/de/nvbw/base/BFRKApiApplicationconfiguration.java
@@ -26,7 +26,7 @@ public class BFRKApiApplicationconfiguration extends Applicationconfiguration {
 		if(File.separator.equals("\\"))
 			configuration_filename = "C:\\Users\\SEI\\eclipse-workspace\\BFRK_API\\bfrk_api.properties";
 		else
-			configuration_filename = "/home/NVBWAdmin/tomcat-deployment/bfrk_api_home/bfrk_api.properties"; 
+			configuration_filename = "/daten/NVBWAdmin/bfrk_api_home/bfrk_api.properties";
 
 		if(debugoutput)
 			System.out.println("configuration_filename ===" + configuration_filename+ "===");
